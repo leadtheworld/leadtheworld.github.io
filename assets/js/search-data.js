0808dab1e058d563099ab0f1519833a3f191e35a
leadtheworld <138272608+leadtheworld@users.noreply.github.com>
2026-07-06 15:55:38 +0000

    Deploying to gh-pages from @ leadtheworld/leadtheworld.github.io@3de7bfe4c2d84b20cb53b3586155b479ba5c79ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,15 +9,15 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-",
-          title: "",
+  },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-projects",
+          title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
