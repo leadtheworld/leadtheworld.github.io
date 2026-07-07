6131082212f0d3de1bcf8226aac1381a19676599
leadtheworld <138272608+leadtheworld@users.noreply.github.com>
2026-07-07 16:51:47 +0000

    Deploying to gh-pages from @ leadtheworld/leadtheworld.github.io@68cf23cd586abf5a00d89cb44fa5ebc2def35822 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "CV page",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
